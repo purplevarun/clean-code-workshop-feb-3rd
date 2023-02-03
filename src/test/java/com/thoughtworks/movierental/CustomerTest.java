fd291468f281650e711692b859f7883f7d711ff3
purplevarun <purplevarun@gmail.com>
2023-02-03 16:34:43 +0530

    Refactor. String with TextBlock in Test

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -11,12 +11,13 @@ class CustomerTest {
         Rental regularMovieRental = new Rental(new Movie("Regular Movie", Movie.REGULAR), 3);
         Rental newReleaseMovieRental = new Rental(new Movie("New Release Movie", Movie.NEW_RELEASE), 2);
         Rental childrensMovieRental = new Rental(new Movie("Children's Movie", Movie.CHILDRENS), 4);
-        String expectedStatement = "Rental Record for Customer\n" +
-                "\tRegular Movie\t3.5\n" +
-                "\tNew Release Movie\t6.0\n" +
-                "\tChildren's Movie\t3.0\n" +
-                "Amount owed is 12.5\n" +
-                "You earned 4 frequent renter points";
+        String expectedStatement = """
+                Rental Record for Customer
+                \tRegular Movie\t3.5
+                \tNew Release Movie\t6.0
+                \tChildren's Movie\t3.0
+                Amount owed is 12.5
+                You earned 4 frequent renter points""";
 
         customer.addRental(regularMovieRental);
         customer.addRental(newReleaseMovieRental);
