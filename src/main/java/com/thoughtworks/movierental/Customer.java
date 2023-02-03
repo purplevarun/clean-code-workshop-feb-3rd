969807b58c346f02493750a63168ce9bca340bf3
purplevarun <purplevarun@gmail.com>
2023-02-03 15:39:23 +0530

    Refactor. Move rental behaviours to Rental class

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -23,41 +23,21 @@ public class Customer {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
-        for (Rental each : rentals) {
-            double thisAmount = 0;
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
-
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" +
-                    thisAmount + "\n";
-            totalAmount += thisAmount;
+        for (Rental rental : rentals) {
+            double rentalAmount = rental.amount();
+            frequentRenterPoints += rental.frequentRenterPoints();
+            result = getResult(result, rental, rentalAmount);
+            totalAmount += rentalAmount;
         }
-
-        //add footer lines result
         result += "Amount owed is " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints
                 + " frequent renter points";
+        return result;
+    }
+
+    private String getResult(String result, Rental rental, double rentalAmount) {
+        result += "\t" + rental.getMovie().getTitle() + "\t" +
+                rentalAmount + "\n";
         return result;
     }
 }
--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -3,6 +3,7 @@ package com.thoughtworks.movierental;
 public class Rental {
     private int daysRented;
     private Movie movie;
+    int frequentRenterPoints = 0;
 
     public Rental(Movie movie, int daysRented) {
         this.movie = movie;
@@ -15,5 +16,32 @@ public class Rental {
 
     public Movie getMovie() {
         return movie;
+    }
+
+    public int frequentRenterPoints() {
+        frequentRenterPoints++;
+        if (movie.getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
+
+    public double amount() {
+        double thisAmount = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
     }
 }
